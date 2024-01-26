4766002fa327b092f833d6df6d6bf633616fde52
vibikerski <vibikerski@gmail.com>
2024-01-26 20:21:50 +0100

    Migrations

--- main.py
+++ main.py
@@ -1,1 +1,2 @@
 import django_setup
+from school_timetable.models import Subject, Teacher, Class, Student
--- school_timetable/models.py
+++ school_timetable/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class Subject(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    birth_year = models.IntegerField()
+    position = models.CharField(max_length=150)
+
+    subject = models.ForeignKey(Subject, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return f'{self.name} {self.surname}'
+
+
+class Class(models.Model):
+    title = models.CharField(max_length=150)
+
+    teacher = models.ForeignKey(Teacher, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.title
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    birth_year = models.IntegerField()
+
+    study_class = models.ForeignKey(Class, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return f'{self.name} {self.surname}'
